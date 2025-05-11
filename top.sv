d7681f6e0b81976fb2ed812a7132f9ff4349f658
Koji Adachi <adachi6k@gmail.com>
2025-05-11 23:36:32 +0900

    blink led1~6

--- top.sv
+++ top.sv
@@ -1,10 +1,20 @@
 module top (
-    input  wire clk,
-    output wire led
+    input  logic clk,
+    output logic led1,
+    output logic led2,
+    output logic led3,
+    output logic led4,
+    output logic led5,
+    output logic led6
 );
-    reg [24:0] counter = 0;
-    always @(posedge clk)
+    logic [29:0] counter = 0;
+    always_ff @(posedge clk)
         counter <= counter + 1;
 
-    assign led = counter[24]; // 約0.75Hz点滅（27MHz想定）
+    assign led1 = ~counter[24]; // 約0.75Hz点滅（27MHz想定）
+    assign led2 = ~counter[25];
+    assign led3 = ~counter[26];
+    assign led4 = ~counter[27];
+    assign led5 = ~counter[28];
+    assign led6 = ~counter[29];
 endmodule
